132c6ad4723dbb6d14be4ad5360bdbe447bba85b
Rafaella <weissrafaella@gmail.com>
2021-12-12 14:37:19 -0300

    Métodos

--- classesAndObjects/Program.cs
+++ classesAndObjects/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using Classes.Heranca;
+
+namespace Classes {
+
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            Ponto p1 = new Ponto(10, 20);
+            Ponto p2 = new Ponto3D(10, 20, 30);
+
+            
+        }
+    }
+}
+
