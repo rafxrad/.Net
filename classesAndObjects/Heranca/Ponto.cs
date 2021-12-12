feccaed520c998d0e983abc9d3d05c01758fccc2
Rafaella <weissrafaella@gmail.com>
2021-12-12 14:28:03 -0300

    first commit

---
+++ classesAndObjects/Heranca/Ponto.cs
@@ -0,0 +1,36 @@
+namespace classesAndObjects
+{
+    public class Ponto
+    {
+        public int x, y;
+        private int distancia;
+
+        public Ponto (int x, int y){
+            x = this.x;
+            y = this.y;
+        }
+
+        protected void CalcularDistancia()
+        {
+
+        }
+
+        private void CalcularDistancia2()
+        {
+
+        }
+
+        public virtual void CalcularDistancia3()
+        {
+            //
+        }
+
+
+
+    }
+}
+
+// public: qualquer parte do código pode acessar
+// protected: apenas as classes que herdam têm acesso
+// internal: só é acessado de dentro do assembly que está contido (projects)
+// private: único e exclusivamente dentro da classe que está contido
