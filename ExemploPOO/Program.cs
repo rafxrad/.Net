7d8018eddec9b619921dfcfebff999e2ffbf3485
Rafaella <weissrafaella@gmail.com>
2021-12-17 15:11:24 -0300

    POO com C# - Abstração na prática

---
+++ ExemploPOO/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace ExemploPOO.Models{
+    class Program {
+        static void Main(string[] args)
+        {
+            Pessoa p1 = new Pessoa();
+            p1.Nome = "Rafa";
+            p1.Idade = 28;
+
+            p1.Apresentar();
+        }
+    }
+}
